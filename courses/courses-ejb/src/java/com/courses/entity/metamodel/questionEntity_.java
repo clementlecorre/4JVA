df4ea3e03dc8b066b744da9b974d353a7eb9ef36
Clément <clc@cloud-temple.com>
2017-01-15 19:51:49 +0100

    add all meta model

---
+++ courses/courses-ejb/src/java/com/courses/entity/metamodel/questionEntity_.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.courses.entity.metamodel;
+
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+/**
+ *
+ * @author clement
+ */
+@StaticMetamodel(questionEntity_.class)
+public class questionEntity_ {
+    public static volatile SingularAttribute<questionEntity_, Integer> idCourses;
+    public static volatile SingularAttribute<questionEntity_, Integer> idQuestion;
+    public static volatile SingularAttribute<questionEntity_, String> responseList;
+}
